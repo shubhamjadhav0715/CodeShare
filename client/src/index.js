05489db400b8b4b17224c792e6d0466b9736c26e
shubhamjadhav0715 <shubhamjadhav0715@gmail.com>
2025-12-10 19:59:58 +0530

    Create React entry point with Redux Provider

---
+++ client/src/index.js
@@ -0,0 +1,49 @@
+/**
+ * React Entry Point
+ * Initializes the React app with Redux store and routing
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import './index.css';
+import App from './App';
+import store from './redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+            success: {
+              duration: 3000,
+              iconTheme: {
+                primary: '#10b981',
+                secondary: '#fff',
+              },
+            },
+            error: {
+              duration: 4000,
+              iconTheme: {
+                primary: '#ef4444',
+                secondary: '#fff',
+              },
+            },
+          }}
+        />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
