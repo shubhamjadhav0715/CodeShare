10e97f8d768eed5291d38008c4146a7a42b182d2
shubhamjadhav0715 <shubhamjadhav0715@gmail.com>
2025-12-12 13:59:18 +0530

    Create PrivateRoute component for protected routes

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+/**
+ * PrivateRoute Component
+ * Protects routes that require authentication
+ * Redirects to login if user is not authenticated
+ */
+
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
